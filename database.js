3fc34b19e6141356f7df5dbe7724a1df057b4697
Dennis Gustafsson <dennisgust@gmail.com>
2021-10-21 11:08:52 +0200

    one to many db for quizes

--- database.js
+++ database.js
@@ -11,25 +11,42 @@ let db = new sqlite3.Database(DBSOURCE, (err) => {
     }else{
         console.log('Connected to the SQlite database.')
         db.run(`CREATE TABLE quizes (
-            quizId INTEGER PRIMARY KEY,
-            quizName TEXT,
-            quizQuestion TEXT,
-            quizCorrectanswer TEXT,
-            quizAnswer1 TEXT,
-            quizAnswer2 TEXT,
-            quizAnswer3 TEXT,
-            quizAnswer4 TEXT
+            quiz_id INTEGER PRIMARY KEY,
+            quiz_name TEXT UNIQUE)`,(err) => {
+            if (err) {
+                // Table already created
+            }else{
+                // Table just created, creating some rows
+                var insertQuizes = 'INSERT INTO quizes (quiz_name) VALUES (?)'
+                db.run(insertQuizes, ["Multiplication Quiz"])
+                db.run(insertQuizes, ["Division Quiz"])
+                }
+        })
+
+        db.run(`   
+            CREATE TABLE questions (
+            question_id INTEGER PRIMARY KEY,
+            question TEXT,
+            correctanswer TEXT,
+            answer1 TEXT,
+            answer2 TEXT,
+            answer3 TEXT,
+            answer4 TEXT,
+            quiz_id INTEGER NOT NULL,
+            FOREIGN KEY (quiz_id) REFERENCES quizes (quiz_id)
             )`,(err) => {
-        if (err) {
-            // Table already created
-        }else{
-            // Table just created, creating some rows
-            var insert = 'INSERT INTO quizes (quizName, quizQuestion, quizCorrectanswer, quizAnswer1, quizAnswer2, quizAnswer3, quizAnswer4) VALUES (?,?,?,?,?,?,?)'
-            db.run(insert, ["Multiplications Quiz","Hur mycket är 10x10?","100","120", "99", "101", "0"])
-            db.run(insert, ["Multiplications Quiz","Hur mycket är 1x10?","10","12", "9", "103", "0"])
-            db.run(insert, ["Division Quiz","Hur mycket är 1x10?","10","12", "9", "103", "0"])
-        }
-    })  
+            if (err) {
+                console.log("in err question")
+                // Table already created
+            }else{
+                console.log("in NOerr question")
+                // Table just created, creating some rows
+                var insertQuestions = 'INSERT INTO questions (question, correctanswer, answer1, answer2, answer3, answer4, quiz_id) VALUES (?,?,?,?,?,?,?)'
+                db.run(insertQuestions, ["Hur mycket är 10x10?","100","120", "99", "101", "0",1])
+                db.run(insertQuestions, ["Hur mycket är 1x10?","10","12", "9", "103", "0",1])
+                db.run(insertQuestions, ["Hur mycket är 1x10?","10","12", "9", "103", "0",2])
+            }
+        })
     }
 })
 
